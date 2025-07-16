89cd9321f18c54425eaa555d2a12f8ecc0478ed3
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-07-16 15:09:59 -0700

    update instructions

--- static/js/createInstructions.js
+++ static/js/createInstructions.js
@@ -3,7 +3,7 @@ is an object to be pushed to the instrTimeline. You'll mainly have to replace th
 the stimulus property. The type will be "html-button-response" if you're using a button
 or "html-keyboard-response" for keys. Check jsPsych to see more configurations. */
 
-const CONTINUE = '<p class="continue" style="font-size: 1.2em; ">[Press SPACE to continue]</p>'; // instruction page footer
+const CONTINUE = '<p class="continue" style="text-align: center;"><br/>[Press SPACE to continue]</p>'; // instruction page footer
 
 const createInstructions1 = function() {
   let instrTimeline = []
@@ -12,13 +12,14 @@ const createInstructions1 = function() {
   instrTimeline.push({
     type: jsPsychFullscreen,
     fullscreen_mode: true,
-    message: '<p style="text-align: left; line-height: 1.6em">'
+    message: '<p>'
       + 'Welcome to this experiment! Before we begin, ensure the following:<br/>'
       + '(1) Use a computer and a modern browser (Chrome, Edge, Firefox, or Safari)<br/>'
       + '(2) Close or minimize other programs and turn off all notifications<br/>'
       + '(3) Silence your phone and minimize background noise<br/>'
       + '(4) Do not exit full screen during the experiment<br/>'
-      + 'If you agree to participate and understand the requirements, click to start:' + '</p>',
+      + 'If you agree to participate and understand the requirements, click to start:' 
+      + '</p>',
     button_label: 'Click here to start in fullscreen',
     delay_after: 100,
     on_finish: function () {
@@ -30,8 +31,8 @@ const createInstructions1 = function() {
   })
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<h3 style="font-size: 2em; ">Instruction 1/6</h3>'
-    + '<p style="text-align: left; line-height: 1.6em; font-size: 1.2em; max-width: 800px; margin: 0 auto; word-wrap: break-word;">'
+    stimulus: '<h3>Instruction 1/6</h3>'
+    + '<p>'
     + 'In this experiment, you will see a series of images on the screen.<br/><br/>'
     + 'You can respond to each image by pressing one of the following buttons on the keyboard:'
     + '</p>'
@@ -42,8 +43,8 @@ const createInstructions1 = function() {
   });
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<h3 style="font-size: 2em; ">Instruction 2/6</h3>'
-    + '<p style="text-align: left; line-height: 1.6em; font-size: 1.2em; max-width: 800px; margin: 0 auto; word-wrap: break-word;">'
+    stimulus: '<h3>Instruction 2/6</h3>'
+    + '<p>'
     + 'Pressing a button in response to an image can give you <span style="color:red;">0</span> or <span style="color:green;">+1</span> points<br/>'
     + 'Your goal is to <b>collect as many points as possible!</b><br/><br/>'
     + 'How often you receive <span style="color:red;">0</span> or <span style="color:green;">+1</span> points <b>depends on each button and each image</b>: '
@@ -53,8 +54,8 @@ const createInstructions1 = function() {
   });
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<h3 style="font-size: 2em; ">Instruction 3/6</h3>'
-    + '<p style="text-align: left; line-height: 1.6em; font-size: 1.2em; max-width: 800px; margin: 0 auto; word-wrap: break-word;">'
+    stimulus: '<h3>Instruction 3/6</h3>'
+    + '<p>'
     + 'When the image is shown, you have <b>two seconds</b> to respond - make sure to answer in that time, or the trial is <b>lost!</b><br/>'
     + 'You will see a + sign between each trial. Keep your eyes on it to be ready to focus on the next image!<br/>'
     + '</p></div>' + CONTINUE,
@@ -62,8 +63,8 @@ const createInstructions1 = function() {
   });
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<h3 style="font-size: 2em; ">Instruction 4/6</h3>'
-    + '<p style="text-align: left; line-height: 1.6em; font-size: 1.2em; max-width: 800px; margin: 0 auto; word-wrap: break-word;">'
+    stimulus: '<h3>Instruction 4/6</h3>'
+    + '<p>'
     + 'For each image you will learn all four buttons on the keyboard.<br/><br/>'
     + 'However, you will not always be allowed to press all four buttons. You will see which are available under the image.<br/>'
     + 'For example, in this trial, only F and H are available - make sure you choose between <b>those two only</b>.<br/>'
@@ -74,8 +75,8 @@ const createInstructions1 = function() {
   });
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<h3 style="font-size: 2em; ">Instruction 5/6</h3>'
-    + '<p style="text-align: left; line-height: 1.6em; font-size: 1.2em; max-width: 800px; margin: 0 auto; word-wrap: break-word;">'
+    stimulus: '<h3>Instruction 5/6</h3>'
+    + '<p>'
     + 'There will be 12 blocks, each with just one image to learn about. You can take short breaks between each block.<br/><br/>'
     + 'Each block will have just one image, but to make sure you are paying attention, we will also show you occasionally an instruction to <b>press a specific button</b>, like shown below.<br/>'
     + 'Make sure you <b>press the correct button</b>.<br/>'
@@ -85,8 +86,8 @@ const createInstructions1 = function() {
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
     stimulus: '<h3 style="font-size: 2em; ">Instruction 6/6</h3>'
-    + '<p style="text-align: left; line-height: 1.6em; font-size: 1.2em; max-width: 800px; margin: 0 auto; word-wrap: break-word;">'
-    + 'You will now start the main experiment.<br/>'
+    + '<p>'
+    + 'You will now start the main experiment.<br/><br/>'
     + 'Before you start, please <b>position your dominant hand with one finger on each button</b>, and keep it there for the block duration!<br/>'
     + '</p></div>' + CONTINUE,
     choices: [" "],
@@ -99,8 +100,8 @@ const createBlockInstructions1 = function(condition, blockCount) {
 
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: `<h3 style="font-size: 2em; ">Image ${blockCount}</h3>`
-    + '<p font-size: 1.2em;">'
+    stimulus: `<h3 style="font-size: 2em; ">Image ${blockCount}/12</h3>`
+    + '<p>'
     + 'For the next trials, only single actions will be available.'
     + '</p></div>' + CONTINUE,
     choices: [" "],
@@ -113,7 +114,7 @@ const createBlockInstructions2 = function(condition, blockCount, allowedKeys) {
   let instrTimeline = []
   let htmlString = `
     <div style="text-align: center; font-size: 1.2em;">
-      <h3 style="font-size: 2em; ">Image ${blockCount}</h3>
+      <h3>Image ${blockCount}/12</h3>
       The following actions will be available:<br/><br/>
       <div style="display: flex; justify-content: center; gap: 40px;">`;
 
@@ -134,10 +135,10 @@ const createTestInstructions = function() {
   instrTimeline = [{
     type: jsPsychHtmlKeyboardResponse,
 	  stimulus: '<h3 style="font-size: 2em; ">Final Phase</h3>'
-    + '<p style="text-align: left; line-height: 1.6em; font-size: 1.2em; max-width: 800px; margin: 0 auto; word-wrap: break-word;">'
-    + 'In this last part of the experiment, we are testing what you learned!<br/>'
-    + 'You will see all the images again, in mixed order, and will make choices like before. You will not see your points, but we are still tracking them!<br/>'
-    + 'All four buttons will be available at all times - press the button that you feel goes with the image. Even if you think you do not remember, go with your instinct!<br/>'
+    + '<p>'
+    + 'In this last part of the experiment, we are <b>testing what you learned!</b><br/><br/>'
+    + 'You will see all the images again, in mixed order, and will make choices like before. You will not see your points, but we are still tracking them!<br/><br/>'
+    + '<b>All four buttons</b> will be available at all times - press the button that you feel goes with the image. Even if you think you do not remember, <b>go with your instinct!</b><br/>'
     + '</p></div>' + CONTINUE,
 	  choices: [" "]
   }]
@@ -149,21 +150,22 @@ const createEndInstructions = function() {
   // Final trial showing thank-you message and saving data
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
-    stimulus: '<h3>Experiment Complete</h3><p>Thank you for your participation!</p><p>Press SPACE to finish</p>',
+    stimulus: '<h3>Experiment Complete</h3>'
+    + '<p style="text-align: center;">Thank you for your participation!<br/>'
+    + 'Press SPACE to finish</p>',
     choices: [" "],
-    on_finish: () => {
+    on_start: () => {
       const id = jsPsych.data.get().values()[1].id || 'unknown';
       const d = new Date(), ymd = d.toISOString().slice(0,10).replace(/-/g, '');
       jsPsych.data.get().localSave('csv', `${id}-${ymd}.csv`);
-    },
-    trial_duration: 5000
+    }
   });
 
   // Exit fullscreen and show mouse cursor again
   instrTimeline.push({
     type: jsPsychFullscreen,
     fullscreen_mode: false,
-    message: '<p style="text-align: left; line-height: 1.6em">You can now close this window.',
+    message: '<p  style="text-align: center;>You can now close this window.</p>',
     on_finish: function () {
       var bodyNode = document.getElementsByTagName("body");
       for (let i = 0; i < bodyNode.length; i++) {
