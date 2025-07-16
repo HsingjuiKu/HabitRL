d0d3d518d4e4782ce48c2357ce948d8143861259
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-07-16 14:40:59 -0700

    second batch of pilot data

--- analysis/test_data.py
+++ analysis/test_data.py
@@ -4,31 +4,54 @@ import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
+# Load data
 data_dir = 'data/'
 file_names = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
 dfs = []
 for idx, file_name in enumerate(file_names):
     df = pd.read_csv(os.path.join(data_dir, file_name))
-    df['id'] = idx
+    df['id'] = file_name
     dfs.append(df)
 data = pd.concat(dfs, ignore_index=True)
+train_data = data.loc[data['phase'] == 'training', :]
+test_data = data.loc[data['phase'] == 'test', :]
 
-plt.figure(figsize=(8, 6))
-sns.countplot(data=data.loc[data['phase'] == 'test'], x='action', hue='condition', order=['A1', 'A2', 'A3', 'A4'])
-plt.title('Frequency of Actions by Condition')
-plt.xlabel('Action')
-plt.ylabel('Frequency')
-plt.legend(title='Condition')
+d = train_data.loc[(train_data['id'] == 'unknown-20250715.csv') & (train_data['image'] == '9'), :]
+a = d.groupby(['id', 'image', 'action', 'condition']).size().reset_index(name='count')
+
+# Plot exploration
+action_counts_train = train_data.groupby(['id', 'action', 'condition']).size().reset_index(name='count')
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 6))
+
+conditions = train_data['condition'].unique()
+for i, condition in enumerate(conditions):
+    condition_data = action_counts_train[action_counts_train['condition'] == condition]
+    sns.pointplot(data=condition_data, x='action', y='count', hue='id', dodge=True, ax=axes[i])
+    axes[i].set_title(f'{condition}')
+    axes[i].set_xlabel('Action')
+    axes[i].set_ylabel('Frequency')
+    axes[i].legend(title='ID', bbox_to_anchor=(1.05, 1), loc='upper left')
+
 plt.tight_layout()
 plt.show()
 
-data_a1 = data.loc[data['action'] == 'A1']
-rew_prob = data.groupby(['action', 'id', 'image'])['reward'].mean()
-plt.figure(figsize=(8, 6))
-sns.histplot(data=rew_prob.reset_index(), x='reward', hue='action', bins=20)
-plt.title('Distribution of Reward Probabilities by Action')
-plt.xlabel('Reward Probability')
-plt.ylabel('Frequency')
+# Plot main effect
+action_counts = test_data.groupby(['id', 'action', 'condition']).size().reset_index(name='count')
+action_counts['proportion'] = action_counts['count'] / 48
+
+fig, axes = plt.subplots(1, 2, figsize=(16, 6))
+
+conditions = test_data['condition'].unique()
+for i, condition in enumerate(conditions):
+    condition_data = action_counts[action_counts['condition'] == condition]
+    sns.pointplot(data=condition_data, x='action', y='proportion', hue='id', dodge=True, ax=axes[i])
+    axes[i].set_title(f'Condition {condition}')
+    axes[i].set_xlabel('Action')
+    axes[i].set_ylabel('Relative frequency')
+    axes[i].legend(title='ID', bbox_to_anchor=(1.05, 1), loc='upper left')
+
 plt.tight_layout()
 plt.show()
 
