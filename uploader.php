cd0f6756a45cb95fb3f02071e384e53988a0fa0f
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-07-23 15:33:00 -0700

    update uploader

--- uploader.php
+++ uploader.php
@@ -1,35 +1,34 @@
 <?php
 
-set_time_limit(0);
-ini_set('memory_limit','512M');
-
-
-// temporary names for debugging
-$data_dir  = __DIR__ . '/' . ($_POST['data_dir']  ?? '');
-$file_name =             $_POST['file_name'] ?? '';
-
-header('Content-Type: application/json');
-if (!is_file("$data_dir/$file_name")) {
-  http_response_code(404);
-  echo json_encode(['status'=>'error','msg'=>"File not found: $data_dir/$file_name"]);
-  exit;
+if (isset($_POST['data_dir']) == true)
+{
+    $data_dir = $_POST['data_dir']; // data directory
 }
 
-$folder_id = '329840044741';
-$access_token = 'Q8HhJDk3JnRhBQPeYnoNP7vgcx4AEjlW';
+if (isset($_POST['file_name']) == true)
+{
+    $file_name = $_POST['file_name']; // mturk_id
+}
+
+// temporary names for debugging
+// $data_dir = 'data';
+// $file_name = '';
 
 $url = 'https://upload.box.com/api/2.0/files/content';
-$json = json_encode([
-  'name'   => $file_name,
-  'parent' => ['id' => $folder_id]
-]);
-$params = [
-  'attributes' => $json,
-  'file'       => new \CurlFile("$data_dir/$file_name", 'text/csv', $file_name)
-];
-$headers = [
-  "Authorization: Bearer $access_token"
-];
+$json = json_encode(array(
+'name' => $file_name, // file name to be created on the box -- should be $file_name like mailer.php
+'parent' => array('id' => '329840044741') // enter the URL of the new Box task data folder here
+));
+$params = array(
+'attributes' => $json,
+
+'file'=>new \CurlFile($data_dir.'/'.$file_name, // name (+ directory) of file to be uploaded
+                     'application/json',//'text/csv', // MIME type of file - change based on if your data is .csv or .json
+                     $file_name // name of file in upload data
+                    )
+);
+// $headers = array("Authorization: Bearer Q8HhJDk3JnRhBQPeYnoNP7vgcx4AEjlW"); primary access token
+$headers = array("Authorization: Bearer XXKRqMHHSaqu9seQ8W2egTQRmpaC19I8");
 
 // leave section below alone
 
@@ -37,32 +36,20 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-
-$response     = curl_exec($ch);
+$response = curl_exec($ch);
+echo $response;
 $responseCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-$errNo        = curl_errno($ch);
-$errStr       = curl_error($ch);
+echo $responseCode;
+$errNo = curl_errno($ch);
+$errStr = curl_error($ch);
 curl_close($ch);
 
 // Box API for looking at folder contents
 //curl -i -X GET "https://api.box.com/2.0/folders/0/items" -H "Authorization: Bearer XXKRqMHHSaqu9seQ8W2egTQRmpaC19I8"
 //curl -i -X GET "https://api.box.com/2.0/folders/124070411640/items" -H "Authorization: Bearer XXKRqMHHSaqu9seQ8W2egTQRmpaC19I8"
 
-if ($responseCode >= 200 && $responseCode < 300) {
-  echo json_encode([
-    'status'   => 'ok',
-    'box_code' => $responseCode,
-    'response' => json_decode($response, true)
-  ]);
-} else {
-  http_response_code(500);
-  echo json_encode([
-    'status'  => 'error',
-    'code'    => $responseCode,
-    'message' => $errStr ?: $response
-  ]);
 
 ?>
