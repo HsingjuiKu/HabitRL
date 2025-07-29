7967f1c168ae520d84134666ed8aa735c05f394a
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-07-29 14:29:35 -0700

    2nd pilot analysis

--- analysis/test_data.py
+++ analysis/test_data.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 # Load data
-data_dir = 'data/'
+data_dir = 'data/pilot_2'
 file_names = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
 dfs = []
 for idx, file_name in enumerate(file_names):
@@ -17,9 +17,9 @@ data = pd.concat(dfs, ignore_index=True)
 train_data = data.loc[data['phase'] == 'training', :]
 test_data = data.loc[data['phase'] == 'test', :]
 
-d = train_data.loc[(train_data['id'] == 'unknown-20250715.csv') & (train_data['image'] == '9'), :]
-a = d.groupby(['id', 'image', 'action', 'condition']).size().reset_index(name='count')
-
+# Calculate maximum time_elapsed for each participant
+max_time_per_id = data.groupby('id')['time_elapsed'].max().reset_index(name='max_time_elapsed')
+max_time_per_id['max_time_elapsed'] /= 60000
 
 # Plot number of different actions per image and id in test_data
 unique_actions_per_image = test_data.groupby(['id', 'image'])['action'].nunique().reset_index(name='num_actions')
@@ -32,7 +32,6 @@ ax.set_ylabel('Number of Different Actions')
 plt.xticks(rotation=45)
 plt.tight_layout()
 plt.show()
-
 
 # Plot attention check accuracy
 attention_check_data = data[data['attention_check'] == True]
@@ -67,15 +66,21 @@ plt.tight_layout()
 plt.show()
 
 # Plot main effect
-action_counts = test_data.groupby(['id', 'action', 'condition']).size().reset_index(name='count')
-action_counts['proportion'] = action_counts['count'] / 48
+unique_ids = test_data['id'].unique()
+unique_actions = ['A1', 'A2', 'A3', 'A4']  # Based on the plot
+unique_conditions = test_data['condition'].unique()
+all_combinations = pd.MultiIndex.from_product([unique_ids, unique_actions, unique_conditions], 
+                                             names=['id', 'action', 'condition'])
+action_counts = test_data.groupby(['id', 'action', 'condition']).size()
+action_counts = action_counts.reindex(all_combinations, fill_value=0).reset_index(name='count')
+action_counts['proportion'] = action_counts['count'] / 24
 
 fig, axes = plt.subplots(1, 2, figsize=(16, 6))
 
 conditions = test_data['condition'].unique()
 for i, condition in enumerate(conditions):
     condition_data = action_counts[action_counts['condition'] == condition]
-    sns.pointplot(data=condition_data, x='action', y='proportion', hue='id', dodge=True, ax=axes[i])
+    sns.pointplot(data=condition_data, x='action', y='proportion', hue='id', dodge=True, ax=axes[i], order=['A1', 'A2', 'A3', 'A4'])
     axes[i].set_title(f'Condition {condition}')
     axes[i].set_xlabel('Action')
     axes[i].set_ylabel('Relative frequency')
