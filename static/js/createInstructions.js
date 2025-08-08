1ea1087d545764d7a7ae9afc9670c637976841e0
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-08-08 13:18:17 -0700

    sona id

--- static/js/createInstructions.js
+++ static/js/createInstructions.js
@@ -233,7 +233,7 @@ const createEndInstructions = function(id) {
     },
   });
 
-  const ptq_survey = "https://ucbpsych.qualtrics.com/jfe/form/SV_2ss0E5VtgTG2oZM?id=${e://Field/id}"
+  const ptq_survey = `https://ucbpsych.qualtrics.com/jfe/form/SV_2ss0E5VtgTG2oZM?id=${id}`
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `<p>Thank you for contributing to the advancement of cognitive science!<br><br>` +
