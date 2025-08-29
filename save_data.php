296e131fcdf5f8ae842a98a2757fa12e491c03a6
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-08-29 15:09:18 -0700

    update php

--- save_data.php
+++ save_data.php
@@ -10,28 +10,19 @@ header('Access-Control-Allow-Origin: https://experiments-ccn.berkeley.edu'); // 
 // - file_name: specify the filename of the data being saved, which can include subject id
 // - exp_data: contain the full json/csv data to be saved
 
-if (isset($_POST['exp_data']) == false) {
-    echo('Hello');
+if (!isset($_POST['exp_data'], $_POST['data_dir'], $_POST['file_name'])) {
     exit;
 }
-$exp_data = $_POST['exp_data'];
-
-/* prevent XSS:  */
 $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-if (isset($_POST['data_dir']) == true)
-{
-    $data_dir = $_POST['data_dir']; // data directory
-} else { exit; }
+$data_dir = $_POST['data_dir'];
+$file_name = $_POST['file_name'];
+$exp_data = $_POST['exp_data'];
+$file_path = $data_dir . '/' . $file_name;
 
-if (isset($_POST['file_name']) == true)
-{
-    $file_name = $_POST['file_name']; // mturk_id
-} else { exit; }
-
-// write the file to disk
-// NOTE: you must make the data directory by all users
-// For example, by running `chmod 772` to give a write access to EVERYONE
-file_put_contents($data_dir.'/'.$file_name, $exp_data);
+// append to file if it exists, otherwise create and write
+file_put_contents($file_path, $exp_data . "\n", FILE_APPEND | LOCK_EX);
+echo "data saved"
 exit;
+
 ?>
