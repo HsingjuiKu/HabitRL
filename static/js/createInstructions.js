58c7d759a0dce3385edd1ebc5743bd78b16f2d95
MMittenbuhler <max.mittenbuhler@gmail.com>
2025-10-29 10:37:00 -0700

    testing

--- static/js/createInstructions.js
+++ static/js/createInstructions.js
@@ -202,7 +202,7 @@ const createEndInstructions = function(id) {
       document.body.style.cursor = "default";
     },
   });
-  const ptq_survey = `https://ucbpsych.qualtrics.com/jfe/form/SV_2ss0E5VtgTG2oZM?id=${id}`
+  const ptq_survey = `https://ucbpsych.qualtrics.com/jfe/form/SV_0OjKiAHNAvhIFDM?id=${id}`
   instrTimeline.push({
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `<p>Thank you for contributing to the advancement of cognitive science!<br><br>` +
